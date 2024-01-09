d200a77ed8687c4ec7332805e9b419d44aaf4c46
Stas <stas06272006@gmail.com>
2024-01-09 18:23:13 +0300

    Create name_01.go

---
+++ name_1/name_01.go
@@ -0,0 +1,32 @@
+/*
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("1 + 1 =", 1 + 1)
+}
+
+
+
+/*package main
+
+import "fmt"
+
+func main(){
+	fmt.Println(len("Hello World"))
+	fmt.Println("Hello World"[1])
+	fmt.Println("Hello" + "World")
+}
+*/
+
+package main
+
+import "fmt"
+
+var x string = "Hello World"
+
+func main() {
+	fmt.Println(x)
+}
